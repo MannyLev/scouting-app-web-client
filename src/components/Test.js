7cebb8723677cbe4f14752ca80a13d11a5241b72
MannyLev <94080012+MannyLev@users.noreply.github.com>
2022-09-19 16:52:37 -0400

    Initial Commit

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import "../styles/styles.css"
+
+const Test = ({ message }) => {
+    return (
+        <>
+            <h1 className="text-style">
+                {message}
+            </h1>
+        </>
+    )
+}
+
+export default Test
